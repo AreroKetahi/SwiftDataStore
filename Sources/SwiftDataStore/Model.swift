d59e5156ee8e78890eda3f34de1f864236368683
Arkivili Collindort <isoblack1@outlook.com>
2025-02-02 19:32:25 +0800

    Update Model.swift

--- Sources/SwiftDataStore/Model.swift
+++ Sources/SwiftDataStore/Model.swift
@@ -13,17 +13,14 @@ import SwiftData
 /// - Note: Register this class to your model schema.
 @Model
 public class LocalDataStore: @unchecked Sendable {
-    @inlinable
     public var key: String
     
-    @inlinable
     public var value: Data
     
     /// Create a new `DataStore` by key and `Data` value.
     /// - Parameters:
     ///   - key: Key of the store
     ///   - value: `Data` raw value.
-    @inlinable
     public init(key: String, value: Data) {
         self.key = key
         self.value = value
