c5e0f1b17dcdf5efa2d614388b67df43df95b034
Arkivili Collindort <isoblack1@outlook.com>
2025-02-17 05:14:53 +0800

    Add SwiftUI View Conformance
    Use DSView for read value only,
    DSBinding for binds the value for mutating

---
+++ Sources/SwiftDataStore/LocalDataStore+SwiftUI.swift
@@ -0,0 +1,107 @@
+//
+//  LocalDataStore+SwiftUI.swift
+//  Score-Control-X
+//
+//  Created by Arkivili Collindort on 2025/2/17
+//
+
+import SwiftUI
+import SwiftData
+
+struct DSView<Content, Value>: View where Content: View, Value: Codable & Sendable {
+    @Environment(\.modelContext) var modelContext
+    @Query var stores: [LocalDataStore]
+    
+    var content: (Value) -> Content
+    
+    private var key: String
+    private var defaultValue: Value
+    
+    var value: Value {
+        return try! store.get(as: Value.self)
+    }
+    
+    var store: LocalDataStore {
+        if let store = stores.first(where: { $0.key == key }) {
+            return store
+        } else {
+            let newStore = try! LocalDataStore(key: key, value: defaultValue)
+            modelContext.insert(newStore)
+            return newStore
+        }
+    }
+    
+    var body: some View {
+        content(value)
+    }
+    
+    init(
+        key: String,
+        defaultValue: Value,
+        content: @escaping (Value) -> Content
+    ) {
+        self.content = content
+        self.key = key
+        self.defaultValue = defaultValue
+    }
+    
+    init(
+        with binding: StoreBinding<Value>,
+        content: @escaping (Value) -> Content
+    ) {
+        self.content = content
+        self.key = binding.key
+        self.defaultValue = binding.defaultValue
+    }
+}
+
+struct DSBinding<Content, Value>: View where Content: View, Value: Codable & Sendable {
+    @Environment(\.modelContext) var modelContext
+    @Query var stores: [LocalDataStore]
+    
+    var content: (Binding<Value>) -> Content
+    
+    private var key: String
+    private var defaultValue: Value
+    
+    var binding: Binding<Value> {
+        return .init {
+            try! store.get()
+        } set: { newValue in
+            try! store.write(newValue)
+        }
+    }
+    
+    var store: LocalDataStore {
+        if let store = stores.first(where: { $0.key == key }) {
+            return store
+        } else {
+            let newStore = try! LocalDataStore(key: key, value: defaultValue)
+            modelContext.insert(newStore)
+            return newStore
+        }
+    }
+    
+    var body: some View {
+        content(binding)
+    }
+    
+    init(
+        key: String,
+        defaultValue: Value,
+        content: @escaping (Binding<Value>) -> Content
+    ) {
+        self.content = content
+        self.key = key
+        self.defaultValue = defaultValue
+    }
+    
+    init(
+        with binding: StoreBinding<Value>,
+        content: @escaping (Binding<Value>) -> Content
+    ) {
+        self.content = content
+        self.key = binding.key
+        self.defaultValue = binding.defaultValue
+    }
+}
